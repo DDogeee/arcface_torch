675344acc3ea271794f2bbdf62cdb3b7376387db
DDogeee <83963507+DDogeee@users.noreply.github.com>
2023-06-17 20:00:29 +0700

    Update custom_augment_resume.py

--- configs/custom_augment_resume.py
+++ configs/custom_augment_resume.py
@@ -19,7 +19,7 @@ config.lr = 0.02
 config.verbose = 2000
 config.dali = False
 
-config.rec = "/kaggle/input/celaba"
+config.rec = "/kaggle/input/celeba"
 config.num_classes = 995
 config.num_image = 105248
 config.num_epoch = 5
